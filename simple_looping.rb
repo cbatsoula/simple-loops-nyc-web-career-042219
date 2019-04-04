3ed3f88c7a3a0e4109441bf760dcec39759f1a54
Catherine Batsoula <cbatsoula@gmail.com>
2019-04-04 05:24:50 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,5 +1,3 @@
-# REMEMBER: print your output to the terminal using 'puts'
-
 def loop_iterator(number_of_times)
 counter = 0
 loop do
@@ -40,4 +38,3 @@ def for_iterator(number_of_times)
     puts "Welcome to Flatiron School's Web Development Course!"
   end
 end
-
